c72285a153a5dfa441c497c9e57bccbd0a7aaf5a
DeepakS-Github <deepakanonymous75@gmail.com>
2024-01-20 21:30:38 +0530

    added key in the map function in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -136,8 +136,8 @@ const ProfilePage = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {users.map((user: UserMap) => (
-                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        {users.map((user: UserMap, index: number) => (
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={index}>
                                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     {user.name}
                                 </th>
